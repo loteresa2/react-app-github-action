e00fef6299a85ca82c03f5bccacb9acdee2e3594
Teresa Lo <loteresa2@gmail.com>
2020-08-16 21:46:25 -0400

    fix: demonstration of sematic release based on commit msg

---
+++ release.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    branches: 'master',
+    repositoryUrl: 'https://github.com/loteresa2/react-app-github-action',
+    plugins: [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        '@semantic-release/git',
+    ],
+}
